f391c0727a784f010fac00274bf1a2421a9dd119
Hugo Pereira <pereirahugo.h@gmail.com>
2018-05-16 12:32:45 +1200

    WebSite Construction Version 1
    Creating the view for each templates
    
    [ticket: 1.1.5]

--- Portfolio/views.py
+++ Portfolio/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    context = {}
+    template = 'home.html'
+    return render(request, template, context)
+
+def projects(request):
+    context = {}
+    template = 'projects.html'
+    return render(request, template, context)
+
+def photography(request):
+    context = {}
+    template = 'photography.html'
+    return render(request, template, context)
+
+def about(request):
+    context = {}
+    template = 'about.html'
+    return render(request, template, context)
+
+def contact(request):
+    context = {}
+    template = 'contact.html'
+    return render(request, template, context)
