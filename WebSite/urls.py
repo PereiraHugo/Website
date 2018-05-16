6fec9f6b305cf6e97461a85fef2dd67cea0e4c78
Hugo Pereira <pereirahugo.h@gmail.com>
2018-05-16 13:14:45 +1200

    WebSite Construction Version 1
    Adding all the views from Portfolio to urls.py in WebSite
    
    [ticket: 1.1.8]

--- WebSite/urls.py
+++ WebSite/urls.py
@@ -1,30 +1,11 @@
-"""WebSite URL Configuration
+from django.urls import path, re_path
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path
-"""
-from django.conf.urls import url, include
-from django.contrib import admin
-"""
 from Portfolio import views as portfolio_views
 
 urlpatterns = [
-    url(r'^$', portfolio_views.home, name='home'),
-    url(r'^projects/$', portfolio_views.projects, name='projects'),
-    url(r'^photography/$', portfolio_views.photography, name='photography'),
-    url(r'^about/$', portfolio_views.about, name='about'),
-    url(r'^contact/$', portfolio_views.contact, name='contact'),
+    re_path(r'^$', portfolio_views.home, name='home'),
+    re_path(r'^projects/$', portfolio_views.projects, name='projects'),
+    re_path(r'^photography/$', portfolio_views.photography, name='photography'),
+    re_path(r'^about/$', portfolio_views.about, name='about'),
+    re_path(r'^contact/$', portfolio_views.contact, name='contact'),
 ]
